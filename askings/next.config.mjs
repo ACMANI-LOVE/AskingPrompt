28913d544fad9386104e62c5cb5eab4a67056f46
ACMANI-LOVE <atomos.fee24@gmail.com>
2024-10-06 04:19:53 +0900

    for local Environment

--- askings/next.config.mjs
+++ askings/next.config.mjs
@@ -1,7 +1,15 @@
+const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd
+  ? process.env.NEXT_PUBLIC_BASE_PATH
+  : process.env.NEXT_LOCAL_BASE_PATH;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/AskingPrompt',
-  assetPrefix: '/AskingPrompt/',
+const nextConfig = (isProd) ? {
+  basePath: basePath,
+  assetPrefix: `${basePath}/`,
+  distDir: "out",
+  output: "export",
+  trailingSlash: true,
+} : {
   distDir: "out",
   output: "export",
   trailingSlash: true,
